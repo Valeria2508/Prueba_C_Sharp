b82a9976eaa5d49fc40ef4c2d18d355803699e8a
valeria2508 <valetqm2002@gmail.com>
2024-08-05 07:54:24 -0500

    feat: se crea metodo CastrateAnimal en dog

--- Prueba_CSharp_ValeriaPiedrahita/models/Dog.cs
+++ Prueba_CSharp_ValeriaPiedrahita/models/Dog.cs
@@ -38,9 +38,25 @@ namespace Prueba_CSharp_ValeriaPiedrahita.models
             Bark Volume: {BarkVolume}
             Coat Type: {CoatType}");
         }
-        public void CreateAnimal (){
-
-
+        public void CastrateAnimal (){
+            if (BreedingStatus == false)
+            {
+                Console.WriteLine("El animal YA se encuentra castrado, no se puede reproducir");
+                BreedingStatus = true;
+            }else
+            {
+                Console.WriteLine("El animal aun NO se encuentra castrado, aun se puede reproducir");
+                Console.WriteLine("Desea castrarrlo?");
+                string castrar = Console.ReadLine();
+                if (castrar == "si".ToLower())
+                {
+                    Console.WriteLine("El animal YA se encuentra castrado, no se puede reproducir");
+                    BreedingStatus = true;
+                }else
+                {
+                    Console.WriteLine("El animal aun NO se encuentra castrado, aun se puede reproducir");
+                }
+            }
         }
 
         public void Hairdress(){
