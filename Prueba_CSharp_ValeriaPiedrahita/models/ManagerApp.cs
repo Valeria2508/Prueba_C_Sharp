7ca34a4b97e4fbf8bcbdb1addacef38fe299a045
valeria2508 <valetqm2002@gmail.com>
2024-08-05 09:33:43 -0500

    fix: se modifica los metodos de crear en manager

--- Prueba_CSharp_ValeriaPiedrahita/models/ManagerApp.cs
+++ Prueba_CSharp_ValeriaPiedrahita/models/ManagerApp.cs
@@ -8,11 +8,55 @@ namespace Prueba_CSharp_ValeriaPiedrahita.models
     public class ManagerApp
     {
         public static Dog CreateDog(){
-            return new Dog(1, "Buddy", new DateOnly(2012, 1, 15), "Golden", "Blanco y negro", 4.5, false, "Timido","32","fuerte","pelo mediano");
+
+            Console.WriteLine("Por favor ingrese los siguientes datos del perro");
+            Console.WriteLine("Ingrese el id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre");
+            string name = Console.ReadLine(); 
+            Console.WriteLine("Ingrese la fecha de nacimiento (YYYY-MM-DD)");
+            DateOnly birthdate = DateOnly.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tipo de raza");
+            string breed = Console.ReadLine();
+            Console.WriteLine("Ingrese el color");
+            string color = Console.ReadLine();
+            Console.WriteLine("Ingrese el peso en kilogramos");
+            double weightInKg = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese si el perro se encuentra en estado de castración (true/false)");
+            bool breedingStatus = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Ingrese el tipo de temperamento(Tímido, Normal o Agresivo. )".ToLower());
+            string temperament = Console.ReadLine();
+            Console.WriteLine("Ingrese el número de microchip");
+            string microShipNumber = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Ingrese que tan fuerte lagra".ToLower());
+            string barkVolume = Console.ReadLine();
+            Console.WriteLine("Ingrese el tipo de pelo (Sin pelo, Pelo corto, Pelo mediano, Pelo largo.)".ToLower());
+            string coatType = Console.ReadLine();
+
+            return new Dog(id, name, birthdate, breed, color, weightInKg, breedingStatus, temperament, microShipNumber, barkVolume, coatType);
         }
 
         public static Cat CreateCat(){
-            return new Cat(1, "mia", new DateOnly(2015, 2, 2), "Persa", "Blanco", 32, true, "Pelo largo");
+            Console.WriteLine("Por favor ingrese los siguientes datos del gato");
+            Console.WriteLine("Ingrese el id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre");
+            string name = Console.ReadLine(); 
+            Console.WriteLine("Ingrese la fecha de nacimiento (YYYY-MM-DD)");
+            DateOnly birthdate = DateOnly.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tipo de raza");
+            string breed = Console.ReadLine();
+            Console.WriteLine("Ingrese el color");
+            string color = Console.ReadLine();
+            Console.WriteLine("Ingrese el peso en kilogramos");
+            double weightInKg = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese si el gato se encuentra en estado de castración (true/false)");
+            bool breedingStatus = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Ingrese la longitud del pelo (Sin pelo, Pelo corto, Pelo mediano, Pelo largo)");
+            string furLenght = Console.ReadLine();
+
+            return new Cat(id, name, birthdate, breed, color, weightInKg, breedingStatus, furLenght);
         }
 
         public static void ShowHeader(){
